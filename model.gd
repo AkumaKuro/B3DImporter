bcb2ba2be2f72028d7b6cb9df43b3a8040bda2ca
Phillip Herda <92669275+AkumaKuro@users.noreply.github.com>
2026-05-11 00:26:51 -0400

    Added node tree structure to models
    This will later be refactored into a skeleton as soon as animation tracks work as intended

--- model.gd
+++ model.gd
@@ -2,7 +2,69 @@ extends Node3D
 
 const ByteBuffer := preload("res://byte_buffer.gd").ByteBuffer
 
+class BBTexture:
+	var file: String
+	var flags: int
+	var blend: int
+	var pos: Vector2
+	var scl: Vector2
+	var rot: float
+
+var textures: Array[BBTexture] = []
+
+enum NodeType {
+	PIVOT,
+	BONE,
+	MESH
+}
+
+class BBNode extends Marker3D:
+	@export_storage var type: NodeType
+	@export_storage var parent: BBNode
+
+	@export_storage var keys: Dictionary[int, Transform3D]
+
+
 var has_bones: bool
+
+func process_texs(buffer: ByteBuffer) -> void:
+	while !buffer.eof_reached():
+		var tex := BBTexture.new()
+		tex.file = buffer.get_string()
+		tex.flags = buffer.get_int()
+		tex.blend = buffer.get_int()
+		tex.pos = buffer.get_vec2()
+		tex.scl = buffer.get_vec2()
+		tex.rot = buffer.get_float()
+		textures.append(tex)
+	pass
+
+class BBBrush:
+	var name: String
+	var color: Color
+	var shininess: float
+	var blend: int
+	var fx: int
+	var texture_id: PackedInt32Array
+
+	func _init(n_texs: int) -> void:
+		texture_id = []
+		texture_id.resize(n_texs)
+
+func process_brush(buffer: ByteBuffer) -> void:
+	var n_texs := buffer.get_int()
+	var brushes: Array[BBBrush] = []
+	while !buffer.eof_reached():
+		var brush := BBBrush.new(n_texs)
+		brush.name = buffer.get_string()
+		brush.color = buffer.get_color()
+		brush.shininess = buffer.get_float()
+		brush.blend = buffer.get_int()
+		brush.fx = buffer.get_int()
+		for texture_id: int in n_texs:
+			brush.texture_id[texture_id] = buffer.get_int()
+		brushes.append(brush)
+
 
 func process_model(buffer: ByteBuffer) -> void:
 	var type := buffer.get_type()
@@ -15,41 +77,66 @@ func process_model(buffer: ByteBuffer) -> void:
 		type = buffer.get_type()
 		match type:
 			"TEXS":
-				buffer.get_sub_buffer()
+				process_texs(buffer.get_sub_buffer())
 			"BRUS":
-				buffer.get_sub_buffer()
+				process_brush(buffer.get_sub_buffer())
 			"NODE":
-				process_node(buffer.get_sub_buffer())
+				process_node(buffer.get_sub_buffer(), self)
 			_:
 				printerr("Weird type: %s" % type)
+	hide()
 
-func process_node(buffer: ByteBuffer) -> void:
-	var node_name := buffer.get_string()
-	var tform := buffer.get_tform()
+var bones: Array[BBBone] = []
 
-	var node_type := buffer.get_type()
+class BBBone:
+	var node: BBNode
 
-	match node_type:
+	# Dict [vertex_id: int, weight: float]
+	var bindings: Dictionary[int, float] = {}
+
+func process_bone(buffer: ByteBuffer, parent: BBNode) -> void:
+	var bone := BBBone.new()
+	bone.node = parent
+	while !buffer.eof_reached():
+		var vertex_id := buffer.get_int()
+		var weight := buffer.get_float()
+
+		bone.bindings[vertex_id] = weight
+	bones.append(bone)
+
+
+func process_node(buffer: ByteBuffer, parent: Node3D) -> void:
+	var node := BBNode.new()
+	parent.add_child(node)
+	node.owner = owner
+
+	node.name = buffer.get_string()
+	node.transform = buffer.get_tform()
+
+	var type := buffer.get_type()
+
+	match type:
 		"MESH":
-			parse_mesh(
-				node_name, tform,
-				buffer.get_sub_buffer()
-			)
+			node.type = NodeType.MESH
+			parse_mesh(node, buffer.get_sub_buffer())
 		"BONE":
-			buffer.get_sub_buffer()
+			node.type = NodeType.BONE
+			process_bone(buffer.get_sub_buffer(), node)
 		"PIVO":
+			node.type = NodeType.PIVOT
 			buffer.get_sub_buffer()
 		_:
+			node.type = NodeType.PIVOT
 			buffer.cursor -= 4
 
 	while !buffer.eof_reached():
-		var type := buffer.get_type()
+		type = buffer.get_type()
 		match type:
 			"KEYS":
 				buffer.get_sub_buffer()
 			"NODE":
 				var n := buffer.get_sub_buffer()
-				process_node(n)
+				process_node(n, node)
 			"ANIM":
 				buffer.get_sub_buffer()
 			"SEQS":
@@ -58,9 +145,18 @@ func process_node(buffer: ByteBuffer) -> void:
 				printerr("%s not recognized" % type)
 				return
 
+func process_anim() -> void:
+	var player := AnimationPlayer.new()
+	var lib := AnimationLibrary.new()
+	var anim := Animation.new()
+
+	# TODO parse anim
+
+	lib.add_animation("Main", anim)
+	player.add_animation_library("lib", lib)
+
 func parse_mesh(
-	node_name: String,
-	tform: Transform3D,
+	node: Node3D,
 	buffer: ByteBuffer
 ) -> void:
 
@@ -91,9 +187,8 @@ func parse_mesh(
 				return
 
 	var mesh_instance := MeshInstance3D.new()
-	mesh_instance.name = node_name
 	mesh_instance.mesh = mesh
-	add_child(mesh_instance)
+	node.add_child(mesh_instance)
 	mesh_instance.owner = owner
 
 func process_tris(buffer: ByteBuffer) -> PackedInt32Array:
